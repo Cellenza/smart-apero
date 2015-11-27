e229cafd716146b16f1451770c1704a99a28953e
Guillaume DEMICHELI <30379@supinfo.com>
2015-11-27 17:29:19 +0100

    Add logos

--- SmartApero/MainPage.xaml.cs
+++ SmartApero/MainPage.xaml.cs
@@ -28,8 +28,11 @@ namespace SmartApero
             new Question { Key = "subject", AssociatedMark = "Bonjour, que puis-je faire pour vous ?"},
             new Question { Key = "persons", AssociatedMark = "C'est noté. Combien de personnes participeront à votre {0:subject} ?", Finder = new PersonsFinder()},
             new Question { Key = "alcool", AssociatedMark = "Parfait, j'ai noté que {1:persons} personnes participeront à votre {0:subject}. Souhaitez-vous boire de l'alcool ?", Finder = new GenericFinder() },
-            new Question { Key = "diet", AssociatedMark = "Y a-t-il des régimes particuliers à respecter: Kachère, allale, végétarien ?" , Finder = new GenericFinder()},
-            new Question { Key = "budget", AssociatedMark = "Etes-vous limité à un budget ?" },
+            new Question { Key = "special", AssociatedMark = "Y a-t-il des régimes particuliers à respecter: Kachère, allale, végétarien ?" , Finder = new GenericFinder()},
+            new Question { Key = "budget", AssociatedMark = "êtes-vous limité à un budget ?" },
+            new Question { Key = "type", AssociatedMark = "Plutôt apéro dinatoire ou classique ?" },
+            new Question { Key = "diet", AssociatedMark = "Dois-je ajouter des produits équilibrés ? (Exemple: crudités, salades composées)." },
+            new Question { Key = "child", AssociatedMark = "Y'aura t-il des enfants ?" },
         };
 
         private SpeechRecognizer speechRecognizer;
